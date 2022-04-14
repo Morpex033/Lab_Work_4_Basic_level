76c7d834bd51de19795264d3049b53db0daf42b2
morpex033 <andrey.sharipov1@gmail.com>
2022-04-14 22:50:50 +0300

    Update commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main  {
 
     public static void main(String[] args) {
+        System.out.println("Определить игровые ноутбуки без сенсорного экрана и напечатать все сведения о них.");
         Laptops laptops = new Laptops("Asus",4,8,false,800);
         Laptops laptops1 = new Laptops("HP",4,16,true,1000);
         Laptops laptops2 = new Laptops();
